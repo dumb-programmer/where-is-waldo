44a6acca194f1376156dd8b1c7cf74716a750329
Asad Khan <asadkhan6192@gmail.com>
2022-06-20 22:52:39 +0500

    Use components in App and style App along with defining css resets in index.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import './styles/App.css';
+import "./styles/App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import puzzleImage from "./assets/puzzle.jpg";
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <main>
+        <img src={puzzleImage} alt="puzzle" className="puzzle-img"/>
+      </main>
+      <Footer />
     </div>
   );
 }
