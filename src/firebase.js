272c3ec65deb5559fd8869af0d794afffa66ba2e
Asad Khan <asadkhan6192@gmail.com>
2022-06-30 21:44:52 +0500

    Define getData method for working with firebase realtime database

--- src/firebase.js
+++ src/firebase.js
@@ -3,24 +3,28 @@ import { getDatabase, onValue, ref } from "firebase/database";
 
 const initFirebase = () => {
   const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.FIREBASE_APP_ID,
   };
-
   initializeApp(firebaseConfig);
 };
 
-const getData = async (callback) => {
+const getData = async () => {
   const db = getDatabase();
   const dbRef = ref(db);
-  onValue(dbRef, (snapshot) => {
-    const data = snapshot.val();
-    callback(data);
+  return new Promise((resolve, reject) => {
+    onValue(
+      dbRef,
+      (snapshot) => {
+        resolve(snapshot.val());
+      },
+      (error) => reject(console.log(error))
+    );
   });
 };
 
