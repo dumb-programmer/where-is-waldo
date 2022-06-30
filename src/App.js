fc0f1cbeae02bbe8f873ce4d5a93e0593d7a6fe1
Asad Khan <asadkhan6192@gmail.com>
2022-06-30 21:52:29 +0500

    Use win state to decide which page to render and remove Footer

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import React, { useEffect, useState } from "react";
 import "./styles/App.css";
 import Header from "./components/Header.jsx";
-import Footer from "./components/Footer.jsx";
 import Cursor from "./components/Cursor.jsx";
+import WinnerScreen from "./components/WinnerScreen.jsx";
+import { initFirebase } from "./firebase";
 import puzzleImage from "./assets/images/puzzle.jpg";
 
 function App() {
   const [coordinates, setCoordinates] = useState({ x: 0, y: 0 });
   const [isMouseIn, setIsMouseIn] = useState(false);
   const [isClicked, setIsClicked] = useState(false);
+  const [selected, setSelected] = useState({
+    Waldo: false,
+    "Wizard Whitebeard": false,
+    Wilma: false,
+    Odlaw: false,
+  });
+  const [win, setWin] = useState(false);
 
   useEffect(() => {
     const main = document.querySelector("main");
-    const x = main.clientX - 50;
-    const y = main.clientY - 120;
+    const x = main.pageX - 49;
+    const y = main.pageY - 120;
     setCoordinates({ x, y });
+    initFirebase();
   }, []);
 
   const onMouseMove = (e) => {
     if (!isClicked) {
       setIsMouseIn(true);
-      const x = e.clientX - 50;
-      const y = e.clientY - 120;
+      const x = e.pageX - 49;
+      const y = e.pageY - 120;
       setCoordinates({ x, y });
     }
   };
@@ -33,14 +42,23 @@ function App() {
 
   return (
     <div className="App">
-      <Header />
-      <main onMouseMove={onMouseMove} onMouseLeave={onMouseLeave}>
-        <img src={puzzleImage} alt="puzzle" className="puzzle-img" />
-        {isMouseIn && (
-          <Cursor coordinates={coordinates} setIsClicked={setIsClicked} />
-        )}
-      </main>
-      <Footer />
+      <Header selected={selected} win={win} />
+      {win ? (
+        <WinnerScreen />
+      ) : (
+        <main onMouseMove={onMouseMove} onMouseLeave={onMouseLeave}>
+          <img src={puzzleImage} alt="puzzle" className="puzzle-img" />
+          {isMouseIn && (
+            <Cursor
+              coordinates={coordinates}
+              setIsClicked={setIsClicked}
+              selected={selected}
+              setSelected={setSelected}
+              setWin={setWin}
+            />
+          )}
+        </main>
+      )}
     </div>
   );
 }
