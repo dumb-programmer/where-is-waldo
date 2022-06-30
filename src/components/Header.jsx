f8f4c48d8c139df14e9f6d141498cf34847b95a4
Asad Khan <asadkhan6192@gmail.com>
2022-06-30 21:53:33 +0500

    Add Wilma and grayscale avatars if the character has been found by the user

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,15 @@ import "../styles/Header.css";
 import uniqid from "uniqid";
 import logo from "../assets/images/logo.png";
 import waldo from "../assets/images/waldo.png";
+import wilma from "../assets/images/wilma.png";
 import odlaw from "../assets/images/odlaw.png";
 import wizard from "../assets/images/wizard_whitebeard.png";
-import React from 'react';
+import React from "react";
 
-const Header = () => {
+const Header = ({ selected, win }) => {
   const avatars = [
     { id: uniqid(), src: waldo, alt: "Waldo" },
+    { id: uniqid(), src: wilma, alt: "Wilma" },
     { id: uniqid(), src: odlaw, alt: "Odlaw" },
     { id: uniqid(), src: wizard, alt: "Wizard Whitebeard" },
   ];
@@ -19,14 +21,15 @@ const Header = () => {
         <img src={logo} alt="where is waldo icon" className="logo" />
       </div>
       <div className="avatar-container">
-        {avatars.map((avatar) => (
-          <img
-            key={avatar.id}
-            src={avatar.src}
-            alt={avatar.alt}
-            className="avatar"
-          />
-        ))}
+        {win ||
+          avatars.map((avatar) => (
+            <img
+              key={avatar.id}
+              src={avatar.src}
+              alt={avatar.alt}
+              className={"avatar" + (selected[avatar.alt] ? " grayscale" : "")}
+            />
+          ))}
       </div>
     </header>
   );
