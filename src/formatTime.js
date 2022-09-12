8c5716e525ea538bcaf5979350ac06a4f08408ed
Asad Khan <asadkhan6192@gmail.com>
2022-09-12 22:11:07 +0500

    Change time format

--- src/formatTime.js
+++ src/formatTime.js
@@ -2,7 +2,8 @@ const formatTime = (time) => {
   const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((time % (1000 * 60)) / 1000);
-  return `${hours}:${minutes}:${seconds}:${time || 0}`;
+  const tseconds = Math.floor(time % 1000);
+  return `${hours}:${minutes}:${seconds}:${tseconds || 0}`;
 };
 
 export default formatTime;
